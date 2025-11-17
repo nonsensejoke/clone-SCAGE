cfba1a207864852745968bb6e32651ce57a63099
黑猫白猫 <144326206+nonsensejoke@users.noreply.github.com>
2025-11-17 14:23:24 +0900

    Add files via upload

---
+++ second-install.sh
@@ -0,0 +1,10 @@
+conda install pytorch==2.1.2 torchvision==0.16.2 torchaudio==2.1.2 pytorch-cuda=12.1 -c pytorch -c nvidia
+
+conda install pytorch==2.1.2  -c pytorch
+
+python -m pip install https://data.pyg.org/whl/torch-2.1.0%2Bcu121/pyg_lib-0.4.0%2Bpt21cu121-cp310-cp310-linux_x86_64.whl
+
+conda install -y "mkl<2024.1" "intel-openmp<2024.1"
+
+conda install -c conda-forge torchmetrics
+
